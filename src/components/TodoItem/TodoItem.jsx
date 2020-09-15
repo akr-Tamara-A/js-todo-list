d76fd496a4abf410ca7e293448bea49cdbe4dad6
Tamara A <akira-49-q@yandex.ru>
2020-09-15 18:01:55 +0300

    feat: добавить начальные todo

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -4,7 +4,7 @@ import './TodoItem.css';
 export default function TodoItem(props) {
   return (
     <li className="todo">
-      <p className="todo__text" />
+      <p className="todo__text">{props.text}</p>
       {props.children}
     </li>
   );
