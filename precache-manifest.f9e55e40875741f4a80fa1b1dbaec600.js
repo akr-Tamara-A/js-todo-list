828047a2245fd1eb8723993ef1c8ac12a878b770
Tamara A <akira-49-q@yandex.ru>
2020-10-24 15:51:58 +0300

    Updates

---
+++ precache-manifest.f9e55e40875741f4a80fa1b1dbaec600.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea67aa1e61e8e669fe6037c96a41f96",
+    "url": "/js-todo-list/index.html"
+  },
+  {
+    "revision": "f2c26f03864e83ac3056",
+    "url": "/js-todo-list/static/css/main.acb8e155.chunk.css"
+  },
+  {
+    "revision": "a4fcd7bcb79e9a577c8e",
+    "url": "/js-todo-list/static/js/2.6d70eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/js-todo-list/static/js/2.6d70eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2c26f03864e83ac3056",
+    "url": "/js-todo-list/static/js/main.37801221.chunk.js"
+  },
+  {
+    "revision": "98b3ef9b5bc3d68c4811",
+    "url": "/js-todo-list/static/js/runtime-main.9e61af93.js"
+  }
+]);
